68340242eeba8628aba4e7f2d0d70818e1232cfe
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-06-28 08:23:52 +0200

    dolphin_fvd_p1: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: dolphin Model: TVBOX Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_dolphin_fvd_p1.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_dolphin_fvd_p1-user \
+    omni_dolphin_fvd_p1-userdebug \
+    omni_dolphin_fvd_p1-eng
---
+++ omni_dolphin_fvd_p1.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from dolphin_fvd_p1 device
+$(call inherit-product, device/dolphin/dolphin_fvd_p1/device.mk)
+
+PRODUCT_DEVICE := dolphin_fvd_p1
+PRODUCT_NAME := omni_dolphin_fvd_p1
+PRODUCT_BRAND := TVBOX
+PRODUCT_MODEL := TVBOX
+PRODUCT_MANUFACTURER := dolphin
+
+PRODUCT_GMS_CLIENTID_BASE := android-dolphin
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="dolphin_fvd_p1-eng 7.0 NRD91N 20230311 test-keys"
+
+BUILD_FINGERPRINT := TVBOX/dolphin_fvd_p1/dolphin-fvd-p1:7.0/NRD91N/20230311:eng/test-keys
